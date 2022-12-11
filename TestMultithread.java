5d8209f36e11c72c952b9a91388de36c319fad06
jelanmathewjames <jelanmathewjames1234@gmail.com>
2022-12-11 10:53:37 -0600

    added thread syncronization and multithread programs

---
+++ TestMultithread.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+class TestMultithread{
+	public static void main(String args[]){
+		RandomThread r = new RandomThread();
+		r.start();
+	}
+}
+
+class RandomThread extends Thread{
+	public void run(){
+		try{
+			
+			Random r = new Random();
+			for(int i=0;i<10;i++){
+				int num = r.nextInt(100);
+				if(num%2==0)
+					new EvenThread(num).start();
+				else
+					new OddThread(num).start();
+			}
+		}
+		catch(Exception e){
+			System.out.println("HEllo");
+		}
+	}
+}
+
+class EvenThread extends Thread{
+	private int num;
+	public EvenThread(int num){
+		this.num = num;
+	}
+	public void run(){
+		System.out.println("Square of "+num+" = "+num*num);
+	}
+}
+
+class OddThread extends Thread{
+	private int num;
+	public OddThread(int num){
+		this.num = num;
+	}
+	public void run(){
+		System.out.println("Cube of "+num+" = "+num*num*num);
+	}
+}
