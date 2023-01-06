d9c74f5c645371a138178a1cc42d3145ee518559
jelanmathewjames <jelanmathewjames1234@gmail.com>
2023-01-06 11:35:16 -0600

    updated Calculator

--- Calculator.java
+++ Calculator.java
@@ -3,20 +3,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator implements ActionListener{
-	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bdiv, bmul, bsub, badd, bdec, beq, bdel, bclr, clear, dot;
+	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bdiv, bmul, bsub, badd, beq, clear, dot;
 	JTextField jtf;
   Calculator() {
 	
     JFrame jrm = new JFrame();
-    jrm.setSize(500, 500);
+    jrm.setSize(350, 450);
 
     JLabel jbl = new JLabel("Calculator");
-    jbl.setBounds(227, 10, 400, 30);
+    jbl.setBounds(130, 10, 400, 30);
     jrm.add(jbl);
-
+	
+	JPanel jp = new JPanel(new GridLayout(5,4),true);
+	jp.setBounds(20,100,300,300);
+	jrm.add(jp);
+	
     jtf = new JTextField();
     jtf.setEditable(false);
-    jtf.setBounds(50, 50, 400, 30);
+    jtf.setBounds(20, 50, 300, 30);
     jrm.add(jtf);
 
     b1 = new JButton("1");
@@ -33,51 +37,28 @@ public class Calculator implements ActionListener{
     bmul = new JButton("X");
     bsub = new JButton("-");
     badd = new JButton("+");
-    bdec = new JButton(".");
     beq = new JButton("=");
     clear = new JButton("Clear");
     dot = new JButton(".");
 
-    b7.setBounds(90, 100, 50, 40);
-    b8.setBounds(180, 100, 50, 40);
-    b9.setBounds(270, 100, 50, 40);
-    bdiv.setBounds(360, 100, 50, 40);
-
-    b4.setBounds(90, 170, 50, 40);
-    b5.setBounds(180, 170, 50, 40);
-    b6.setBounds(270, 170, 50, 40);
-    bmul.setBounds(360, 170, 50, 40);
-
-    b1.setBounds(90, 240, 50, 40);
-    b2.setBounds(180, 240, 50, 40);
-    b3.setBounds(270, 240, 50, 40);
-    bsub.setBounds(360, 240, 50, 40);
-
-    bdec.setBounds(90, 310, 50, 40);
-    b0.setBounds(180, 310, 50, 40);
-    beq.setBounds(270, 310, 50, 40);
-    badd.setBounds(360, 310, 50, 40);
-    clear.setBounds(200, 400, 90, 40);
-    dot.setBounds(250, 470, 50, 40);
-
-    jrm.add(b7);
-    jrm.add(b8);
-    jrm.add(b9);
-    jrm.add(bdiv);
-    jrm.add(b4);
-    jrm.add(b5);
-    jrm.add(b6);
-    jrm.add(bmul);
-    jrm.add(b1);
-    jrm.add(b2);
-    jrm.add(b3);
-    jrm.add(bsub);
-    jrm.add(bdec);
-    jrm.add(b0);
-    jrm.add(beq);
-    jrm.add(badd);
-    jrm.add(clear);
-    jrm.add(dot);
+    jp.add(b7);
+    jp.add(b8);
+    jp.add(b9);
+    jp.add(bdiv);
+    jp.add(b4);
+    jp.add(b5);
+    jp.add(b6);
+    jp.add(bmul);
+    jp.add(b1);
+    jp.add(b2);
+    jp.add(b3);
+    jp.add(bsub);
+    jp.add(b0);
+    jp.add(beq);
+    jp.add(badd);
+    jp.add(clear);
+    jp.add(dot);
+    
 
     jrm.setLayout(null);
     jrm.setResizable(false);
@@ -115,26 +96,22 @@ public class Calculator implements ActionListener{
           Float x, y, ans = 0f;
           x = Float.parseFloat(S.substring(0, i));
           y = Float.parseFloat(S.substring(i + 1, S.length()));
-          if (b == '+') {
+          if (b == '+') 
             ans = x + y;
-          } else if (b == '-') {
+          else if (b == '-')
             ans = x - y;
-          } else if (b == 'X') {
+          else if (b == 'X')
             ans = x * y;
-          } else if (b == '/') {
+          else if (b == '/') 
             ans = x / y;
-          }
-          if (b == '/' && y == 0) {
+          if (b == '/' && y == 0)
             jtf.setText("Not defined!");
-          } else {
+          else
             jtf.setText(ans + "");
-          }
-        } catch (Exception ex) {
+        }catch (Exception ex) {
           jtf.setText("Enter two operands!");
         }
      }else if(e.getSource() == clear){
-     	if (jtf.getText().equals("Enter two operands!") || jtf.getText().equals("Not defined!"))
-          jtf.setText("");
         jtf.setText("");
      }else{
      	JButton source = (JButton)e.getSource();
@@ -142,10 +119,8 @@ public class Calculator implements ActionListener{
           jtf.setText("");
         jtf.setText(jtf.getText() + source.getText());
      }
-  
   }
   public static void main(String[] args) {
     new Calculator();
   }
-
 }
